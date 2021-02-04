b0c7de05fe9fe49179f4754037044e7344bafdd9
Linus045 <lwillms88@gmail.com>
2021-02-04 14:34:40 +0100

    Added output for selling with different pools

--- main.py
+++ main.py
@@ -68,6 +68,37 @@ def addLossGainToBalance(asset, amount, isGain):
     else:
         Balance[asset] -= amount
 
+def getPriceForSymbol(amount, symbol):
+    if symbol in current_prices:
+        price = current_prices[symbol]
+        return amount * price
+    
+    print('Error: No pricelist for ' + symbol + ' or ' + symbol_reversed)
+    raise
+
+def getPriceForAssetPair(amount, baseAsset, quoteAsset):
+    symbol = baseAsset + quoteAsset
+    symbol_reversed = quoteAsset + baseAsset
+
+    if symbol in current_prices:
+        price = current_prices[symbol]
+        return amount * price
+
+    if symbol_reversed in current_prices:
+        price = current_prices[symbol_reversed]
+        return amount / price
+    
+    print('Error: No pricelist for ' + symbol + ' or ' + symbol_reversed)
+    raise
+
+def printCurrentPrice(symbol):
+    if symbol in current_prices:
+        print(symbol, current_prices[symbol])
+    else:
+        print('Error: No pricelist for ' + symbol)
+        raise
+
+
 def getPriceInEur(amount, asset):
     if asset == 'EUR':
         return amount;
@@ -109,10 +140,10 @@ def getBalanceAsString(old_balance, ignoreEmpty=True, alsoInEuro=False, onlyShow
     for key in Balance:
         if not onlyShowCurrencyThatChanged or onlyShowCurrencyThatChanged and (balance_diff[key] != 0):
             if not ignoreEmpty or ignoreEmpty and (Balance[key] != 0 or balance_diff[key] != 0):
-            balance_as_string += '|' + key.ljust(padding) + ': ' + formatNumber(Balance[key])
-            balance_in_eur = getPriceInEur(Balance[key], key)
-            total_in_eur += balance_in_eur
-            if alsoInEuro:
+                balance_as_string += '|' + key.ljust(padding) + ': ' + formatNumber(Balance[key])
+                balance_in_eur = getPriceInEur(Balance[key], key)
+                total_in_eur += balance_in_eur
+                if alsoInEuro:
                     balance_as_string += ' (' + formatNumber(balance_in_eur,2) + '€)'
                 balance_as_string += '|'
                 if (key in balance_diff) and (balance_diff[key] != 0):
@@ -124,48 +155,96 @@ def getBalanceAsString(old_balance, ignoreEmpty=True, alsoInEuro=False, onlyShow
     balance_as_string += '\n|Total in EUR: '.ljust(padding) + formatNumber(total_in_eur, precision=8, showSign=False) + '€|'
     return balance_as_string
 
+def calculateHistoricalOrders():
+    output_file = open("output.log", "w", encoding="utf-8")
+    output_file.write("")
+    output_file.close()
 
-output_file = open("output.log", "w", encoding="utf-8")
-output_file.write("")
-output_file.close()
+    output_file = open("output.log", "a", encoding="utf-8")
 
-output_file = open("output.log", "a", encoding="utf-8")
+    for order in allOrders:
+        if (order['status'] == 'FILLED') and (order['isWorking'] == True):
+            symbol = order['symbol']
+            symbolAssets = symbolToAssetsDict[symbol]
+            baseAsset = symbolAssets['baseAsset']
+            quoteAsset = symbolAssets['quoteAsset']
+            
+            base_digitPrecision = symbolAssets['baseAssetPrecision']
+            quote_digitPrecision = symbolAssets['quoteAssetPrecision']
 
-for order in allOrders:
-    if (order['status'] == 'FILLED') and (order['isWorking'] == True):
-        symbol = order['symbol']
-        symbolAssets = symbolToAssetsDict[symbol]
-        baseAsset = symbolAssets['baseAsset']
-        quoteAsset = symbolAssets['quoteAsset']
-        
-        base_digitPrecision = symbolAssets['baseAssetPrecision']
-        quote_digitPrecision = symbolAssets['quoteAssetPrecision']
+            price = float(order['price'])
+            quantity = float(order['executedQty'])
+            quoteQty = float(order['cummulativeQuoteQty'])
+            old_balance = Balance.copy()
+            if order['side'] == 'BUY':
+                    addLossGainToBalance(baseAsset, quantity, True)
+                    addLossGainToBalance(quoteAsset, quoteQty, False)
+            elif order['side'] == 'SELL':
+                    addLossGainToBalance(baseAsset, quantity, False)
+                    addLossGainToBalance(quoteAsset, quoteQty, True)
 
-        price = float(order['price'])
-        quantity = float(order['executedQty'])
-        quoteQty = float(order['cummulativeQuoteQty'])
-        old_balance = Balance.copy()
-        if order['side'] == 'BUY':
-                addLossGainToBalance(baseAsset, quantity, True)
-                addLossGainToBalance(quoteAsset, quoteQty, False)
-        elif order['side'] == 'SELL':
-                addLossGainToBalance(baseAsset, quantity, False)
-                addLossGainToBalance(quoteAsset, quoteQty, True)
+            orderTime = datetime.utcfromtimestamp(int(order['time']) / 1000)
+            orderTimeAsString = orderTime.strftime("%d/%m/%Y - %H:%M:%S")
+            orderUpdateTime = datetime.utcfromtimestamp(int(order['updateTime']) / 1000)
+            orderUpdateTimeAsString = orderTime.strftime("%d/%m/%Y - %H:%M:%S")
 
-        orderTime = datetime.utcfromtimestamp(int(order['time']) / 1000)
-        orderTimeAsString = orderTime.strftime("%d/%m/%Y - %H:%M:%S")
-        orderUpdateTime = datetime.utcfromtimestamp(int(order['updateTime']) / 1000)
-        orderUpdateTimeAsString = orderTime.strftime("%d/%m/%Y - %H:%M:%S")
+            text = '-------ORDER: ' + str(order['orderId']) + '-----' + '\n'
+            text = orderTimeAsString  + ' (Updated:' + orderUpdateTimeAsString + ')'+ '\n'
+            text += symbol;
+            text += ' ' + order['side'] + '@' + formatNumber(price, base_digitPrecision) + '  Base Amount traded:' + str(quantity) + ' - '
+            text += 'BaseAsset: ' + baseAsset + ' QuoteAsset: ' + quoteAsset
+            text += ' (Quote amount received:'+ str(quoteQty) +')' + '\n'
+            text += getBalanceAsString(old_balance, True, True, False) + '\n'
+            text += '-----------------'+ '\n'
+            output_file.write(text);
+            print(text)
+    output_file.close()
 
-        text = '-------ORDER: ' + str(order['orderId']) + '-----' + '\n'
-        text = orderTimeAsString  + ' (Updated:' + orderUpdateTimeAsString + ')'+ '\n'
-        text += symbol;
-        text += ' ' + order['side'] + '@' + formatNumber(price, base_digitPrecision) + '  Base Amount traded:' + str(quantity) + ' - '
-        text += 'BaseAsset: ' + baseAsset + ' QuoteAsset: ' + quoteAsset
-        text += ' (Quote amount received:'+ str(quoteQty) +')' + '\n'
-        text += getBalanceAsString(old_balance, True, True, False) + '\n'
-        text += '-----------------'+ '\n'
-        output_file.write(text);
-        print(text)
+def printPriceWithConversions(startAmount, assetList):
+    print('--------------------------------')
+    print('->'.join(assetList))
+    value = startAmount
+    fee = 0
+    for i in range(len(assetList)):
+        print('Asset', assetList[i],'Value', formatNumber(value), 'Fee', formatNumber(fee))
+        if i  < (len(assetList) - 1):
+            value = getPriceForAssetPair(value, assetList[i], assetList[i + 1]) 
+            if assetList[i + 1] != 'EUR' and assetList[i] != 'EUR':
+                fee = value * 0.1 / 100  # substract fee
+            else:
+                fee = 0
+            value -= fee
+    return value
+            
 
-output_file.close()
+
+
+coinBalance = 0 #amount of coins (leave at 0 to pull from binance)
+coin = 'DOGE' # will automatically pull the balance from binance if coinBalance is 0
+includeLocked = True # will include balance that is currently locked in an order
+
+#traiding courses in sell order (need to start with coin)
+pairs = [
+    ['DOGE', 'EUR'],
+    ['DOGE', 'BTC', 'EUR'],
+    ['DOGE', 'BTC', 'ETH', 'EUR']
+]
+
+if coinBalance == 0:
+    coinBalance = client.get_asset_balance(coin)
+coinAmount = float(coinBalance['free'])
+if includeLocked:
+    coinAmount += float(coinBalance['locked'])
+
+highestValue = -1
+highestIdx = -1
+for idx in range(len(pairs)):
+    value = printPriceWithConversions(coinAmount, pairs[idx])
+    if value > highestValue:
+        highestValue = value
+        highestIdx = idx
+
+if highestIdx >= 0:
+    print('================')
+    print('Highest pair', '->'.join(pairs[highestIdx]), formatNumber(highestValue))
+
